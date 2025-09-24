b38e1b74ff65cc14bd12d472f41a69c83496d77e
denwaya34 <denwaya34@gmail.com>
2025-09-24 12:31:37 +0900

    add types node

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
