fce662ee163274b221a1c95454d64489387c77f4
denwaya34 <denwaya34@gmail.com>
2025-09-24 12:07:53 +0900

    add types node

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
